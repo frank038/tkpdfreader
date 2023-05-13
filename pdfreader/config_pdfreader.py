b464c1bf2ad0fe3d2f1e160b6336f70c8ecf8252
frank038 <42263206+frank038@users.noreply.github.com>
2023-05-13 15:56:06 +0200

    Update config_pdfreader.py

--- pdfreader/config_pdfreader.py
+++ pdfreader/config_pdfreader.py
@@ -2,7 +2,7 @@
 starting_zoom=2.5
 # font size of the application
 font_size=12
-# use 1 to set custom paper colour otherwise 0
+# use 1 to set custom paper colour, otherwise 0
 USE_ALPHA=0
 # background colour in the form #xxxxxx or #xxx or x colour (example: gray90)
 PAGE_BG="gray90"
