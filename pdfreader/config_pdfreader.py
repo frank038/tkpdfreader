1ab0d247545eac51376879c11e4b54e5674b6299
frank038 <42263206+frank038@users.noreply.github.com>
2023-05-13 15:53:23 +0200

    Add files via upload

---
+++ pdfreader/config_pdfreader.py
@@ -0,0 +1,10 @@
+# the starting zoom level
+starting_zoom=2.5
+# font size of the application
+font_size=12
+# use 1 to set custom paper colour otherwise 0
+USE_ALPHA=0
+# background colour in the form #xxxxxx or #xxx or x colour (example: gray90)
+PAGE_BG="gray90"
+# right pad for the horizontal scrollbar
+hsb_pad=16
